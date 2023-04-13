311e39353c94a8b0d872b65695176e80f4ad977e
Jorge Lemes <jorgelemes85@outlook.com.br>
2023-04-13 10:36:57 -0300

    udemy
    treinamento

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
+
 import './App.css';
+import Comentario from './components/Comentario';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     
+        <h1> Meu projeto </h1>
+        <Comentario nome="Jorge" email="jorge@email.com" data={new Date(2023, 4, 13)}> 
+        Olá, tudo bem?
+        </Comentario > 
+
+        <hr />
+        <br />
+        
+        <Comentario nome="Jorge" email="jorge@email.com" data={new Date(2023, 4, 13)}> 
+        Olá, tudo bem?
+        </Comentario > 
+        
+      
     </div>
   );
 }
